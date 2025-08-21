ab9d1fe5888a7b93c7ef9d771a63d1633c4b9d3c
aarti bhosale <aartibhosalepatil@gmail.com>
2025-08-21 18:03:24 +0530

    filtering applied for email and attachment tables

--- mail/admin.py
+++ mail/admin.py
@@ -6,9 +6,11 @@ class EmailAdmin(admin.ModelAdmin):
     list_display = ('subject', 'from_email', 'to','timestamp','user','email_type')
     list_per_page = 10
     search_fields = ('subject', 'from_email', 'to')
+    list_filter = ('email_type', 'timestamp')
 
 
 @admin.register(Attachment)
 class AttachmentAdmin(admin.ModelAdmin):
     list_display = ('email','filename','file')
+    list_filter = ('email__user', 'email__timestamp')
 
