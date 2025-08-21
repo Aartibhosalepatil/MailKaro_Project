5a7f3e9e90a249a03b7e3f058980eb41887f165c
aarti bhosale <aartibhosalepatil@gmail.com>
2025-08-21 15:20:56 +0530

    admin list dispaly fields added to nd email_type

--- mail/admin.py
+++ mail/admin.py
@@ -3,7 +3,7 @@ from .models import Email,Attachment
 
 @admin.register(Email)
 class EmailAdmin(admin.ModelAdmin):
-    list_display = ('subject', 'from_email', 'timestamp','user')
+    list_display = ('subject', 'from_email', 'to','timestamp','user','email_type')
     list_per_page = 10
 
 @admin.register(Attachment)
